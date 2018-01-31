37a76914915b23f1156912639b049a3175a05891
baby snaps <jmw686@nyu.edu>
2018-01-31 14:29:07 -0600

    adding todo deletions

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,8 +2,8 @@
   <div>
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-   // we are now passing the data to the todo component to render the todo list
-    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
+   // we are now passing the data to the todo component to render the todo list. not a comment?
+    <todo  v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
   </div>
 </template>
 
@@ -15,6 +15,12 @@ export default {
   props: ['todos'],
   components: {
     Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
   },
 };
 </script>
