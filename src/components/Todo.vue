499988c7129804c15da378caa3c3384a15c9c8b5
baby snaps <jmw686@nyu.edu>
2018-01-31 14:22:13 -0600

    add some editing states

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,23 +1,44 @@
 <template>
   <div class='ui centered card'>
     <div class='content'>
-        <div class='header'>
+        <div class='header' v-show="!isEditing">
             {{ todo.title }}
         </div>
         <div class='meta'>
             {{ todo.project }}
         </div>
         <div class='extra content'>
-            <span class='right floated edit icon'>
+            <span class='right floated edit icon' v-on:click="showForm">
             <i class='edit icon'></i>
           </span>
         </div>
     </div>
-    <div class='ui bottom attached green basic button' v-show="todo.done">
+    // form shown only when editing
+    <div class="content" v-show="isEditing">
+      <div class="ui form">
+        <div class="field">
+          <label>Title</label>
+          <input type='text' v-model="todo.title">
+        </div>
+        <div class="field">
+          <label>Project</label>
+          <input type='text' v-model="todo.project">
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click="hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
         Completed
     </div>
-    <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
+    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
+        Pending
     </div>
 </div>
 </template>
