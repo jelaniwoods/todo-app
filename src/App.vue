22433b5c88798e8777c759f657c8f0241c57aba5
baby snaps <jmw686@nyu.edu>
2018-01-31 14:31:17 -0600

    add createTodo to components

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
 
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -12,7 +12,30 @@ import TodoList from './components/TodoList'
 export default {
   name: 'App',
   components: {
-    TodoList
+    TodoList,
+    CreateTodo
+  },
+  // data function pushes data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }]
+    }
   }
 }
 </script>
