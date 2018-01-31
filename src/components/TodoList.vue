af5e10d5babaddc97ddf4f7b7fd4bee6bae62766
baby snaps <jmw686@nyu.edu>
2018-01-31 14:11:56 -0600

    utilize Todo component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <ul>
-      <li> Todo A </li>
-      <li> Todo B </li>
-      <li> Todo C </li>
-    </ul>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+   // we are now passing the data to the todo component to render the todo list
+    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
-  export default {}
-</script>
 
-<style></style>
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+};
+</script>
